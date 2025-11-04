22aeb709f34287fc8a64f6b88796e543e11fce73
Pritam Kumar Patel <pritamkumarpatel1212@gmail.com>
2025-11-05 02:08:57 +0530

    Varieables and Case senstivity

---
+++ Basics/case_Sensetive.py
@@ -0,0 +1,5 @@
+Var = "This is capital Var"
+var = "This is small var"
+
+print(Var)
+print(var)
---
+++ Basics/varieables.py
@@ -0,0 +1,9 @@
+name = "pritam"
+age = 22
+height = 5.7
+is_student = True
+
+print("Name:", name)
+print("Age:", age)      
+print("Height:", height)
+print("Is Student:", is_student)
