22068fdc26579867878c99a081b0f1bdfd65f405
Pritam Kumar Patel <pritamkumarpatel1212@gmail.com>
2025-11-15 02:42:49 +0530

    Use case of pass

---
+++ 8_Loop/pass.py
@@ -0,0 +1,18 @@
+for i in range(5):
+    pass
+
+
+
+# We can use pass in if statement also
+
+if (i <= 5):
+    pass
+
+
+
+# Example of pass statement in for loop
+for i in range(1, 6):
+    if (i == 3):
+        pass  # do nothing
+    else:
+        print(i)
