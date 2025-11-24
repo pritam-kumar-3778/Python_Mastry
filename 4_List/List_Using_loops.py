2e0fadeeb9d9560b7da1fda76b74c174234c5717
Pritam Kumar Patel <pritamkumarpatel1212@gmail.com>
2025-11-24 23:39:49 +0530

    Tuple List with loop

---
+++ 4_List/List_Using_loops.py
@@ -0,0 +1,4 @@
+list = [1,3,4,6,7,4.9, 6.9]
+
+for el in list:
+    print(el)
---
+++ 5_Tuple/Tuple_with_Loop.py
@@ -0,0 +1,10 @@
+tup = (3,5,6,4,9.6,)
+
+# Calculate sum of all value
+
+sum = 0
+
+for el in tup:
+    sum += el
+
+print(f"Sum of value is : {sum}")
